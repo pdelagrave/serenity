b549d5acb2d490a22076993639e87ce75e68f519
Pierre Delagrave <pierre.delagrave@gmail.com>
2023-06-19 22:21:59 -0400

    Files cleanup

--- Userland/Applications/Bits/BitsWidget.cpp
+++ Userland/Applications/Bits/BitsWidget.cpp
@@ -9,12 +9,83 @@
 #include "PeersTabWidget.h"
 #include "Userland/Applications/Bits/LibBits/Engine.h"
 #include "Userland/Applications/Bits/LibBits/MetaInfo.h"
+#include "Userland/Applications/Bits/LibBits/Net/PeerContext.h"
+#include "Userland/Applications/Bits/LibBits/Net/PieceHeap.h"
+#include "Userland/Applications/Bits/LibBits/Net/TorrentContext.h"
+#include "Userland/Applications/Bits/LibBits/Torrent.h"
+#include <AK/NumberFormat.h>
 #include <LibFileSystemAccessClient/Client.h>
 #include <LibGUI/Action.h>
 #include <LibGUI/BoxLayout.h>
 #include <LibGUI/Menu.h>
 #include <LibGUI/SortingProxyModel.h>
 #include <LibGUI/Splitter.h>
+
+int TorrentModel::row_count(GUI::ModelIndex const&) const
+{
+    return m_torrents.size();
+}
+int TorrentModel::column_count(GUI::ModelIndex const&) const
+{
+    return Column::__Count;
+}
+GUI::Variant TorrentModel::data(GUI::ModelIndex const& index, GUI::ModelRole role) const
+{
+    if (role == GUI::ModelRole::TextAlignment)
+        return Gfx::TextAlignment::CenterLeft;
+    if (role == GUI::ModelRole::Display) {
+        auto torrent = Bits::Engine::s_engine->torrents().at(index.row());
+        Bits::MetaInfo& meta_info = torrent->meta_info();
+        auto tcontext = m_torrents.at(index.row());
+
+        switch (index.column()) {
+        case Column::Name:
+            return torrent->display_name();
+        case Column::Size:
+            return AK::human_readable_quantity(meta_info.total_length());
+        case Column::State:
+            return state_to_string(torrent->state()).release_value_but_fixme_should_propagate_errors();
+        case Column::Progress:
+            return DeprecatedString::formatted("{:.1}%", torrent->state() == Bits::TorrentState::CHECKING ? torrent->check_progress() : tcontext->local_bitfield.progress());
+        case Column::DownloadSpeed:
+            return DeprecatedString::formatted("{}/s", human_readable_size(tcontext->download_speed));
+        case Column::UploadSpeed:
+            return DeprecatedString::formatted("{}/s", human_readable_size(tcontext->upload_speed));
+        case Column::Path:
+            return torrent->data_path();
+        default:
+            VERIFY_NOT_REACHED();
+        }
+    }
+    return {};
+}
+
+void TorrentModel::update()
+{
+    m_torrents = m_get_updated_torrent_list();
+    did_update(UpdateFlag::DontInvalidateIndices);
+}
+String TorrentModel::column_name(int column) const
+{
+    switch (column) {
+    case Column::Name:
+        return "Name"_short_string;
+    case Column::Size:
+        return "Size"_short_string;
+    case Column::State:
+        return "State"_string.release_value();
+    case Column::Progress:
+        return "Progress"_string.release_value();
+    case Column::DownloadSpeed:
+        return "Download Speed"_string.release_value();
+    case Column::UploadSpeed:
+        return "Upload Speed"_string.release_value();
+    case Column::Path:
+        return "Path"_string.release_value();
+    default:
+        VERIFY_NOT_REACHED();
+    }
+}
 
 ErrorOr<void> BitsWidget::open_file(String const& filename, NonnullOwnPtr<Core::File> file, bool start)
 {
--- Userland/Applications/Bits/BitsWidget.h
+++ Userland/Applications/Bits/BitsWidget.h
@@ -7,11 +7,51 @@
 #pragma once
 
 #include "PeersTabWidget.h"
-#include "TorrentModel.h"
 #include "Userland/Applications/Bits/LibBits/Engine.h"
 #include "Userland/Applications/Bits/LibBits/Torrent.h"
 #include <LibGUI/TableView.h>
 #include <LibGUI/Widget.h>
+
+namespace Bits {
+struct PeerContext;
+struct TorrentContext;
+struct PieceStatus;
+class PieceHeap;
+}
+
+using TorrentListCallback = Function<Vector<NonnullRefPtr<Bits::TorrentContext>>()>;
+
+class TorrentModel final : public GUI::Model {
+public:
+    int row_count(GUI::ModelIndex const& index) const override;
+    int column_count(GUI::ModelIndex const& index) const override;
+    GUI::Variant data(GUI::ModelIndex const& index, GUI::ModelRole role) const override;
+    String column_name(int i) const override;
+
+    static NonnullRefPtr<TorrentModel> create(TorrentListCallback callback)
+    {
+        return adopt_ref(*new TorrentModel(move(callback)));
+    }
+    void update();
+
+private:
+    TorrentModel(TorrentListCallback callback)
+        : m_get_updated_torrent_list(move(callback)) {};
+
+    enum Column {
+        Name,
+        Size,
+        State,
+        Progress,
+        DownloadSpeed,
+        UploadSpeed,
+        Path,
+        __Count
+    };
+
+    TorrentListCallback m_get_updated_torrent_list;
+    Vector<NonnullRefPtr<Bits::TorrentContext>> m_torrents;
+};
 
 class BitsWidget final : public GUI::Widget {
     C_OBJECT(BitsWidget)
